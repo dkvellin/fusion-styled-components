a5b640070f678c911daf9f54ad7b4d417a683ecf
Deepak Kumar vellingiri <aspiring.us@gmail.com>
2017-07-25 16:02:01 +0530

    Adding Badge component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Avatar, Button, Logo, Slider, Heading, BackgroundImage } from './fusion';
+import { Alert, Avatar, Button, Logo, Slider, Heading, BackgroundImage, Badge } from './fusion';
 import { ThemeProvider } from 'styled-components';
 import { primaryTheme, secondaryTheme } from './styles';
 
@@ -22,6 +22,7 @@ class App extends React.Component {
           </ThemeProvider>
           <Heading tag="h2" href="#">Heading</Heading>
           <Logo width={200} />
+          <Heading>Heading<Badge>Badge</Badge></Heading>
           <Slider name="will" min={1} max={10} defaultValue={1} disabled={false} onChange={() => console.log("Change Event")} />
           <BackgroundImage size="cover" position="center"/>
         </div>
